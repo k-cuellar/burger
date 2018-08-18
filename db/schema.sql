951b90ca84262dee17534baeda2141abef7a8a80
kcuellar <katherine.cuellar93@gmail.com>
2018-08-17 21:12:19 -0400

    schema, seeds, connection

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+-- Schema for creating DB
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Initial seeds for data in database
+
+INSERT INTO burgers (burger_name)
+VALUES ("In N' Out");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Whataburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Whopper");
+
